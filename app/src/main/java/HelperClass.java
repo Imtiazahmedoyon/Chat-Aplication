1591351d6a7307870394976dae0ba5716c319c95
Subbroto ray <ray231051014622@diu.edu.bd>
2024-12-08 23:52:45 +0600

    namuser name and password

--- app/src/main/java/HelperClass.java
+++ app/src/main/java/HelperClass.java
@@ -1,8 +1,11 @@
+
+package com.daffodil.diuchat;
 public class HelperClass {
 }
         String name, email, username, password;
 
 public String getName() {
+
     return name;
 }
 
@@ -16,4 +19,30 @@ public String getEmail() {
 
 public void setEmail(String email) {
     this.email = email;
+}
+public String getUsername() {
+    return username;
+}
+
+public void setUsername(String username) {
+    this.username = username;
+}
+
+public String getPassword() {
+    return password;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
+public HelperClass(String name, String email, String username, String password) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+}
+
+public HelperClass() {
+}
 }
