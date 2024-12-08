89564f2413501cb691226d26fa8fda0763133630
Subbroto ray <ray231051014622@diu.edu.bd>
2024-12-08 23:42:31 +0600

    name and email

--- app/src/main/java/HelperClass.java
+++ app/src/main/java/HelperClass.java
@@ -1,2 +1,19 @@
 public class HelperClass {
 }
+        String name, email, username, password;
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getEmail() {
+    return email;
+}
+
+public void setEmail(String email) {
+    this.email = email;
+}
