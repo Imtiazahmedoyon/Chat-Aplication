70891b17bb113c4aa8a801e62f44e8168dbef8cf
Imtiazahmedoyon <ahmed2305101942@diu.edu.bd>
2024-12-09 01:15:49 +0600

    searchxml

--- app/src/main/java/com/daffodil/diuchat/SearchUserActivity.java
+++ app/src/main/java/com/daffodil/diuchat/SearchUserActivity.java
@@ -1,20 +1,33 @@
 package com.daffodil.diuchat;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchUserActivity extends AppCompatActivity {
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search_user);
+
+        searchInput =findViewById(R.id.search_username_input);
+        searchButton=findViewById(R.id.search_user_btn);
+        backButton=findViewById(R.id.back_btn);
+        recyclerView=findViewById(R.id.search_user_recycler_view);
+
 
     }
 }
