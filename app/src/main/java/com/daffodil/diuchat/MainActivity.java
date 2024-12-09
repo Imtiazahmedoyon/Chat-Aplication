438887ee49efec059b1fe18be893aff6c444e8b8
md-ashik-miah <miah2305101945@diu.edu.bd>
2024-12-10 02:49:21 +0600

    main activity

--- app/src/main/java/com/daffodil/diuchat/MainActivity.java
+++ app/src/main/java/com/daffodil/diuchat/MainActivity.java
@@ -1,21 +1,29 @@
 package com.daffodil.diuchat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     BottomNavigationView bottomNavigationView;
     ImageButton searchButton;
 
@@ -25,14 +33,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         chatFragment= new ChatFragment();
         profileFragment= new ProfileFragment();
 
-        bottomNavigationView=findViewById(R.id.bottom_navigaton);
-        searchButton=findViewById(R.id.main_search_btn);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        searchButton = findViewById(R.id.main_search_button);
 
         searchButton.setOnClickListener((v) ->{
             startActivity(new Intent(MainActivity.this,SearchUserActivity.class));
@@ -43,15 +50,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if(item.getItemId()==R.id.menu_chat){
-
-                  getSupportFragmentManager().beginTransaction().replace(R.id.main_framw_layout,chatFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,chatFragment).commit();
                 }
                 if(item.getItemId()==R.id.menu_profile){
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_framw_layout,profileFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,profileFragment).commit();
                 }
-
-
                 return true;
             }
         });
